8504fae1403e521b2de769d9bb3fb93835e08017
Ishan127 <164486869+Ishan-Deepsolv@users.noreply.github.com>
2025-09-19 23:15:38 +0530

    Helo 7

--- src/train/train_ddp.py
+++ src/train/train_ddp.py
@@ -1,4 +1,7 @@
+
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
