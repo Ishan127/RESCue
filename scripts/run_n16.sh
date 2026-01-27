4c907f1715d2fafc2edb603535ee0068b95a1223
Ishan127 <chughishan127@gmail.com>
2026-01-27 23:53:44 +0530

    maybe

--- scripts/run_n16.sh
+++ scripts/run_n16.sh
@@ -7,10 +7,10 @@ echo "Expected time per sample: ~8-12s"
 
 python scripts/evaluate_pipeline.py \
     --max_n 16 \
-    --parallel_requests 4 \
-    --workers_planner 2 \
-    --workers_executor 4 \
-    --workers_verifier 4 \
+    --parallel_requests 32 \
+    --workers_planner 4 \
+    --workers_executor 8 \
+    --workers_verifier 32 \
     --mode comparative \
     --fraction 1.0 \
     "$@"
